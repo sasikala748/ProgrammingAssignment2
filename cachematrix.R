03bce44d8e9dfe24cf19eef05f5ac0557410914b
sasikala748 <sasikala.ramasamy@gmail.com>
2021-01-18 20:42:36 +0530

    The Edited File which contains the solution for the special matrix function in Rstudio

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCachematrix creates a matrix that could cache its inverse
+## get the matrix to inverse
 
-## Write a short comment describing this function
+## the function stores the matrix and caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-null
+  set<-function(y){
+      x<<-y
+      inv<<-NULL
+  }
+  get<-function()x
+  setInverse<-function(inverse)inv<<-inverse
+  getInverse<-function()inv
+  list(set=set,get=get,
+       setInverse=setInverse,
+       getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## This cacheSolve function gives inverse of the matrix returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv<-x$getInverse()
+  if(!is.null(inv)){
+      message("getting cached data")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
